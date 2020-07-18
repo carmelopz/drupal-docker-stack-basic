2da523c8f208cf3d6a3adcff5a1b0d917a3cdb2b
carmelopz <carmelopz@gmail.com>
2020-07-18 17:01:05 +0200

    Create settings.local.php

---
+++ settings/settings.local.php
@@ -0,0 +1,9 @@
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'host' => 'database',
+  'username' => 'drupal8',
+  'password' => 'drupal8',
+  'database' => 'drupal8',
+  'prefix' => '',
+);
+
